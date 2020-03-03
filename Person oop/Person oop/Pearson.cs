0a66d4e930ebd0e648d5b0ae2321b85fd9b76b46
Zie Mcdowell <59741700+ZieMcd@users.noreply.github.com>
2020-03-03 13:45:07 +0200

    Add files via upload

---
+++ Person oop/Person oop/Pearson.cs
@@ -0,0 +1,90 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person_oop
+{
+    class Pearson
+    {
+        private string name;
+        private string indentity;
+        private string contact;
+        private string address;
+        private string email;
+
+        public Pearson(string aName, string aIndentity, string aContact, string aAdress, string aEmail)
+        {
+            this.name = aName;
+            this.indentity = aIndentity;
+            this.contact = aContact;
+            this.address = aAdress;
+            this.email = aEmail;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if ( value != null)
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public string Id
+        {
+            get { return this.indentity; }
+            set
+            {
+                if (indentity != null)
+                {
+                    indentity = value;
+                }
+            }
+        }
+
+        public string Contact
+        {
+            get { return this.contact; }
+            set
+            {
+                if (value != null)
+                {
+                    this.contact = value;
+                }
+            }
+        }
+
+        public string Adress
+        {
+            get { return this.address; }
+            set
+            {
+                if (value != null)
+                {
+                    this.address = value;
+                }
+            }
+        }
+
+        public string Email
+        {
+            get { return this.email; }
+            set
+            {
+                if (value != null)
+                {
+                    this.email = value;
+                }
+            }
+        }
+
+
+        public string Display()
+        {
+            return "Name: "+this.name +"\nIdentity: "+this.indentity+"\nContact: "+this.contact+"\nAdress: "+this.address + "\nEmail: "+this.email;
+        }
+    }
+}
---
+++ Person oop/Person oop/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Person_oop;
+
+namespace Person_oop
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Pearson[] person = new Pearson[3];
+            Pearson[] loaded = Load(person);
+
+            for (int i = 0; i<loaded.Length; i++)
+            {
+                Console.WriteLine(loaded[i].Display());
+            }
+         
+ 
+            //Console.WriteLine(zie.Email);
+        }
+
+        static Pearson[] Load(Pearson[] person)
+        {
+
+            string name, id, contact, address, email;
+
+            for (int i = 0; i<person.Length; i++)
+            {
+                Console.Write("Enter Name: ");
+                name = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("Enter ID: ");
+                id = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("Enter Phone Number: ");
+                contact = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("Enter Town: " );
+                address = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("Enter Email: ");
+                email = Console.ReadLine();
+
+                person[i] = new Pearson(name,id,contact,address,email);
+            }
+            return person;
+        }
+
+
+        
+
+        
+    }
+}
